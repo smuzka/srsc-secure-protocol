629babe98b2bf115f805905ec96daf9372211811
Stanisław Pietryga <staspietryga@gmail.com>
2024-10-25 12:58:57 +0100

    Working example with TestMulticast

--- src/main/java/Utils/ToHex.java
+++ src/main/java/Utils/ToHex.java
@@ -7,18 +7,16 @@ public class ToHex {
     /**
      * Return string hexadecimal from byte array of certain size
      *
-     * @param data : bytes to convert
+     * @param data   : bytes to convert
      * @param length : nr of bytes in data block to be converted
-     * @return  hex : hexadecimal representation of data
+     * @return hex : hexadecimal representation of data
      */
 
-    public static String toHex(byte[] data, int length)
-    {
+    public static String toHex(byte[] data, int length) {
         StringBuffer buf = new StringBuffer();
 
-        for (int i = 0; i != length; i++)
-        {
-            int	v = data[i] & 0xff;
+        for (int i = 0; i != length; i++) {
+            int v = data[i] & 0xff;
 
             buf.append(digits.charAt(v >> 4));
             buf.append(digits.charAt(v & 0xf));
@@ -27,4 +25,14 @@ public class ToHex {
         return buf.toString();
     }
 
+    // Converts a hexadecimal string to a byte array
+    public static byte[] fromHex(String hex) {
+        int len = hex.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
+                    + Character.digit(hex.charAt(i + 1), 16));
+        }
+        return data;
+    }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -16,17 +16,91 @@ import javax.crypto.spec.SecretKeySpec;
 
 import srscProject.src.main.java.Utils.RandomBytesGenerator;
 import srscProject.src.main.java.Utils.ToHex;
-
+import srscProject.src.main.java.Utils.ReadFile;
 
 public class main {
-    public static void printMessage() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, ShortBufferException, IllegalBlockSizeException, BadPaddingException {
+    static Cipher cipher;
+    static SecretKeySpec key;
+    static IvParameterSpec ivSpec;
+    static byte[] ivBytes;
+
+    public static void init()
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+            InvalidKeyException, ShortBufferException, IllegalBlockSizeException, BadPaddingException {
 
         Map<String, String> variables = ReadFile.getVariables(new String[0]);
 
         String confidentiality = variables.get("CONFIDENTIALITY");
         String[] confidentialityParts = confidentiality.split("/");
 
-//        Generate Key
+        // Generate Key
+        byte[] keyBytes = RandomBytesGenerator.getRandomBytes(variables.get("SYMMETRIC_KEY_SIZE"));
+        key = new SecretKeySpec(keyBytes, confidentialityParts[0]);
+        String configSymmetricKey = variables.get("SYMMETRIC_KEY");
+        if (configSymmetricKey != null) {
+            key = new SecretKeySpec(configSymmetricKey.getBytes(StandardCharsets.UTF_8), confidentialityParts[0]);
+        }
+
+        // Generate IV
+        String configIVSize = variables.get("IV Size");
+        ivBytes = new byte[Integer.parseInt(configIVSize)];
+        ivSpec = new IvParameterSpec(ivBytes);
+        String configIV = variables.get("IV");
+        if (configIV != null) {
+            ivSpec = new IvParameterSpec(configIV.getBytes(StandardCharsets.UTF_8));
+        }
+
+        cipher = Cipher.getInstance(confidentiality);
+
+    }
+
+    public static String encryptString(String plainText)
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+            InvalidKeyException, ShortBufferException, IllegalBlockSizeException, BadPaddingException {
+
+        // encryption
+        cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+        byte[] inputBytes = plainText.getBytes(StandardCharsets.UTF_8);
+        System.out.println("Input: " + plainText);
+        byte[] cipherText = new byte[cipher.getOutputSize(ivBytes.length + inputBytes.length)];
+        int ctLength = cipher.update(ivBytes, 0, ivBytes.length, cipherText, 0);
+        ctLength += cipher.update(inputBytes, 0, inputBytes.length, cipherText, ctLength);
+        ctLength += cipher.doFinal(cipherText, ctLength);
+        System.out.println("ciphera: " + ToHex.toHex(cipherText, ctLength) + " bytes: " + ctLength);
+        return ToHex.toHex(cipherText, ctLength).toString();
+    }
+
+    public static String decryptString(String cipherTextHex)
+            throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException,
+            InvalidKeyException, ShortBufferException, IllegalBlockSizeException,
+            BadPaddingException {
+
+        // Convert hex string to byte array
+        byte[] cipherText = ToHex.fromHex(cipherTextHex);
+
+        // decryption
+        cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+        byte[] buf = new byte[cipher.getOutputSize(cipherText.length)];
+        int bufLength = cipher.update(cipherText, 0, cipherText.length, buf, 0);
+        bufLength += cipher.doFinal(buf, bufLength);
+        // remove the iv from the start of the message
+        byte[] plainText = new byte[bufLength - ivBytes.length];
+        System.arraycopy(buf, ivBytes.length, plainText, 0, plainText.length);
+        System.out.println("plain : " + new String(plainText, StandardCharsets.UTF_8) + " bytes: " + plainText.length);
+        return new String(plainText, StandardCharsets.UTF_8);
+    }
+
+    public static void printMessage()
+            throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException,
+            InvalidKeyException, ShortBufferException, IllegalBlockSizeException, BadPaddingException {
+
+        Map<String, String> variables = ReadFile.getVariables(new String[0]);
+
+        String confidentiality = variables.get("CONFIDENTIALITY");
+        String[] confidentialityParts = confidentiality.split("/");
+
+        // Generate Key
         byte[] keyBytes = RandomBytesGenerator.getRandomBytes(variables.get("SYMMETRIC_KEY_SIZE"));
         SecretKeySpec key = new SecretKeySpec(keyBytes, confidentialityParts[0]);
         String configSymmetricKey = variables.get("SYMMETRIC_KEY");
@@ -34,7 +108,7 @@ public class main {
             key = new SecretKeySpec(configSymmetricKey.getBytes(StandardCharsets.UTF_8), confidentialityParts[0]);
         }
 
-//        Generate IV
+        // Generate IV
         String configIVSize = variables.get("IV Size");
         byte[] ivBytes = new byte[Integer.parseInt(configIVSize)];
         IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
@@ -53,7 +127,7 @@ public class main {
         int ctLength = cipher.update(ivBytes, 0, ivBytes.length, cipherText, 0);
         ctLength += cipher.update(input, 0, input.length, cipherText, ctLength);
         ctLength += cipher.doFinal(cipherText, ctLength);
-        System.out.println("cipher: " + ToHex.toHex(cipherText, ctLength) + " bytes: " + ctLength);
+        System.out.println("ciphera: " + ToHex.toHex(cipherText, ctLength) + " bytes: " + ctLength);
 
         // decryption
         cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
@@ -65,7 +139,5 @@ public class main {
         System.arraycopy(buf, ivBytes.length, plainText, 0, plainText.length);
         System.out.println("plain : " + new String(plainText, StandardCharsets.UTF_8) + " bytes: " + plainText.length);
 
-
     }
 }
-
